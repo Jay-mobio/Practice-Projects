a5ee85e61ffce38aa65ce0e77a07afbed4b0f31e
Jay Kapadiya <jayk.mobio@gmail.com>
2023-02-27 10:20:09 +0530

    Created automatic read and transformation pipeline

--- Pipeline/Automation/auto.py
+++ Pipeline/Automation/auto.py
@@ -1,59 +1,40 @@
-import pandas as pd
 import psycopg2
-from sqlalchemy import create_engine
-import os
+import pandas as pd
 
-# Establish a connection to the Postgres database
-conn = psycopg2.connect(
-    host="localhost",
-    database="task",
-    user="postgres",
-    password="1713"
-)
+# PostgreSQL connection details
+host = 'localhost'
+port = '5432'
+dbname = 'task'
+user = 'postgres'
+password = '1713'
 
-# Create a cursor object to interact with the database
+# CSV file details
+csv_file = 'B:\Python\crypto-market-GBP.csv'
+table_name = 'Cryptoo'
+
+# Load data from CSV file using pandas
+df = pd.read_csv(csv_file)
+
+# Get the column names from the CSV file
+cols = list(df.columns)
+df = df.rename(columns={'Unnamed: 0': 'id'})
+cols[0] = df.columns[0]
+df.columns = [col.replace(" ", "_") for col in df.columns]
+df.columns = [col.replace(".", "_") for col in df.columns]
+# Create a connection to PostgreSQL
+conn = psycopg2.connect(host=host, port=port, dbname=dbname, user=user, password=password)
 cur = conn.cursor()
 
-# Define the table schema
-table_name = "Crypttttt"
-cols = ["Year", "Name", "Stars", "Director"]
-col_defs = [f"{col} TEXT" for col in cols]
-create_table_query = f"CREATE TABLE IF NOT EXISTS public.{table_name} ({', '.join(col_defs)});"
-cur.execute(create_table_query)
+# Create the table if it doesn't already exist, with columns matching the CSV file
+cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join([f'{col} VARCHAR(255)' for col in cols])})")
+cur.execute(f"SELECT COUNT(*) FROM {table_name}")
+num_rows = cur.fetchone()[0]
+df = pd.read_csv(csv_file, skiprows=num_rows, nrows=3)
+# Insert data into PostgreSQL table
+for row in df.itertuples(index=False):
+    cur.execute(f"INSERT INTO {table_name} ({', '.join(cols)}) VALUES ({', '.join(['%s']*len(cols))})", row)
 
-# Create a SQLAlchemy engine for bulk insertions
-engine = create_engine("postgresql://postgres:1713@localhost:5432/task")
-
-# Read the CSV file
-file_path = "B:\Python\Games.csv"
-df = pd.read_csv(file_path)
-
-# Initialize the row counter
-if not os.path.exists("row_counter.txt"):
-    with open("row_counter.txt", "w") as f:
-        f.write("0")
-
-with open("row_counter.txt", "r+") as f:
-    row_counter = int(f.read())
-
-# Select the next batch of rows
-start_index = row_counter
-end_index = row_counter + 3
-batch_df = df.iloc[start_index:end_index]
-
-# Transform the data by selecting only the first 3 rows
-transformed_df = batch_df.head(3)
-
-# Load the transformed data into the PostgreSQL database
-transformed_df.to_sql(table_name, engine, if_exists="append", index=False)
-
-# Increment the row counter
-row_counter += 3
-
-# Write the updated row counter to the file
-with open("row_counter.txt", "w") as f:
-    f.write(str(row_counter))
-
-# Close the database connection
+# Commit the changes and close the connection
+conn.commit()
 cur.close()
 conn.close()
